f12db68eeb30ae54ae9aaf26e36e0a798a4edfb7
Admin <max.12.viv@gmail.com>
2026-09-16 21:34:56 +0300

    added cards and placeholders for image, category

--- app/src/main/java/com/example/subscriptionmanager/ui/subscriptionlist/SubscriptionListScreen.kt
+++ app/src/main/java/com/example/subscriptionmanager/ui/subscriptionlist/SubscriptionListScreen.kt
@@ -1,24 +1,33 @@
 package com.example.subscriptionmanager.ui.subscriptionlist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+
 @Composable
+// Main function, draws the whole screen with list of subscriptions
 fun SubscriptionListScreen(
     viewModel: SubscriptionListViewModel = viewModel()
 ) {
@@ -26,18 +35,57 @@ fun SubscriptionListScreen(
     Scaffold { innerPadding ->
         LazyColumn (
             modifier = Modifier.padding(innerPadding),
-            contentPadding = PaddingValues(16.dp),
+            contentPadding = PaddingValues(12.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(subscriptions) { (name, price) ->
-                Card(modifier = Modifier.fillMaxWidth()) {
-                    Column (modifier = Modifier.padding(16.dp)) {
-                        Text(name, style = MaterialTheme.typography.titleMedium)
-                        Text("$$price / month", style = MaterialTheme.typography.bodyMedium)
-                    }
-                }
+            items(subscriptions) { (name, category, price, isActive) ->
+                SubscriptionCard(name, category, price, isActive)
             }
         }
     }
+}
 
+@Composable
+// Function that draws a box for a subscription in the list
+fun SubscriptionCard(
+    name : String,
+    category : String,
+    price : String,
+    isActive : Boolean
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp)
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Placeholder, replace with image
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .background(Color.Black, RoundedCornerShape(12.dp))
+            ) { }
+            Spacer(modifier = Modifier.size(12.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = name)
+                Text(text = category)
+            }
+            Column(
+                horizontalAlignment = Alignment.End
+            ) {
+                Text(text = "$$price")
+                Text(
+                    if (isActive) "Active" else "Inactive"
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/subscriptionmanager/ui/subscriptionlist/SubscriptionListViewModel.kt
+++ app/src/main/java/com/example/subscriptionmanager/ui/subscriptionlist/SubscriptionListViewModel.kt
@@ -5,17 +5,19 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 data class Subscription(
-    val name: String,
-    val price: String
+    val name: String        = "placeholder",
+    val categories: String  = "placeholder",
+    val price: String       = "placeholder",
+    val isActive : Boolean  = true
 )
 
 class SubscriptionListViewModel : ViewModel() {
 
     private val _subscriptions = MutableStateFlow(
         listOf(
-            Subscription("Netflix", "15.99"),
-            Subscription("Spotify", "9.99"),
-            Subscription("Disney+", "7.99")
+            Subscription("Netflix", price = "15.99"),
+            Subscription("Spotify", price = "9.99"),
+            Subscription("Disney+", price = "7.99")
         )
     )
     val subscriptions: StateFlow<List<Subscription>> = _subscriptions
