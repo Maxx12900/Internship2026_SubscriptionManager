9226bfb2f238af611c4070294d0319b658d29549
Admin <max.12.viv@gmail.com>
2026-09-17 09:21:24 +0300

    added dataclass Subscription

---
+++ app/src/main/java/com/example/subscriptionmanager/data/Subscription.kt
@@ -0,0 +1,8 @@
+package com.example.subscriptionmanager.data
+
+data class Subscription(
+    val name : String,
+    val categories : String,
+    val price : String,
+    val isActive : Boolean
+)
