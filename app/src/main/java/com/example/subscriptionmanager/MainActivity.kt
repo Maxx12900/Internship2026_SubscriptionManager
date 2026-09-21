d53628d6c4c3159d54ab57e750135418b20fa10a
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2026-09-21 14:10:25 +0300

    minor changed to notification handling

--- app/src/main/java/com/example/subscriptionmanager/MainActivity.kt
+++ app/src/main/java/com/example/subscriptionmanager/MainActivity.kt
@@ -15,12 +15,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.example.subscriptionmanager.navigation.AppNavGraph
+import com.example.subscriptionmanager.notifications.createNotification
+import com.example.subscriptionmanager.notifications.createNotificationChannel
+import com.example.subscriptionmanager.notifications.showNotification
 import com.example.subscriptionmanager.ui.theme.SubscriptionManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        createNotificationChannel(this)
+
         setContent {
             SubscriptionManagerTheme {
                 AppNavGraph()
