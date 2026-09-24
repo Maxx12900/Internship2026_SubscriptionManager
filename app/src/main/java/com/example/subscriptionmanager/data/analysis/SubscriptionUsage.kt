3aef2e29b9b93283de4be1984d669d67f0c67603
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2026-09-24 14:18:07 +0300

    the UsageStatManager will not request permission if it already has one

--- app/src/main/java/com/example/subscriptionmanager/data/analysis/SubscriptionUsage.kt
+++ app/src/main/java/com/example/subscriptionmanager/data/analysis/SubscriptionUsage.kt
@@ -1,32 +1,41 @@
 package com.example.subscriptionmanager.data.analysis
 
-import android.Manifest
 import android.app.Activity
+import android.app.AppOpsManager
 import android.app.usage.UsageStats
 import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.icu.util.Calendar
+import android.os.Process
 import android.provider.Settings
-import androidx.core.app.ActivityCompat
 import androidx.core.net.toUri
-import kotlin.collections.set
 
-fun getApplicationsUsageData(activity: Activity, intervalMillis: Int): Map<String, UsageStats> {
-    // intervalMillis - how long is the interval you want to analyze (in milliseconds).
-    if (ActivityCompat.checkSelfPermission(
-            activity,
-            Manifest.permission.PACKAGE_USAGE_STATS
-        ) != PackageManager.PERMISSION_GRANTED
-    ) {
+fun requestPermissionForUsageDataAccess(activity: Activity): Int {
+    val appOps = activity.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+
+    val mode = appOps.checkOpNoThrow(
+        AppOpsManager.OPSTR_GET_USAGE_STATS,
+        Process.myUid(),
+        activity.packageName
+    )
+
+    if (mode != AppOpsManager.MODE_ALLOWED) {
         // request permission
         val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS).apply {
             data = "package:${activity.packageName}".toUri()
         }
         activity.startActivity(intent)
+    }
 
-        return mapOf()  // nothing to return
+    return mode
+}
+
+
+fun getApplicationsUsageData(activity: Activity, intervalMillis: Int): Map<String, UsageStats> {
+    // intervalMillis - how long is the interval you want to analyze (in milliseconds).
+
+    if (requestPermissionForUsageDataAccess(activity) != AppOpsManager.MODE_ALLOWED) {
+        return mapOf()  // womp womp
     }
 
     val usageStatsManager = activity.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
