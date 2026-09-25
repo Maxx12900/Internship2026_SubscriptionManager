c2559b6472910f8f138b12e800b58e3256302071
Admin <max.12.viv@gmail.com>
2026-09-25 09:56:28 +0300

    feature: connected addscreen to DB

--- app/src/main/java/com/example/subscriptionmanager/data/entities/Category.kt
+++ app/src/main/java/com/example/subscriptionmanager/data/entities/Category.kt
@@ -2,12 +2,13 @@ package com.example.subscriptionmanager.data.entities
 
 // Category.order are the flags representing
 enum class Category(val value:Long) {
-    STREAMING(1L),
-    PRODUCTIVITY(1L shl 2),
-    GAMES(1L shl 3),
-    FOOD(1L shl 4),
+    NONE(0L),
+    STREAMING(1L shl 0),
+    PRODUCTIVITY(1L shl 1),
+    GAMES(1L shl 2),
+    FOOD(1L shl 3)
 }
 
 infix fun Category.or(other: Category): Long = this.value or other.value
 
-fun Set<Category>.toBitmask(): Long = fold(0) { bitmask, category -> bitmask + category.value}
+fun Set<Category>.toBitmask(): Long = fold(0L) { bitmask, category -> bitmask or category.value}
